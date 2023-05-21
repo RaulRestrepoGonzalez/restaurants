dcb97dfac1b7370394b0dab3cfa0ccbabb299274
RaulRestrepoGonzalez <ralfonsorestrepo@unicesar.edu.co>
2023-05-21 14:44:32 -0500

    creacion de las pruebas unitarias e integracion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,46 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:restaurants/carrito/carrito.dart';
 
 import 'package:restaurants/main.dart';
+import 'package:restaurants/models/cartaMap.dart';
+import 'package:restaurants/models/item.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Añadir al carrrito', (WidgetTester tester) async {
+    final carrito = Carrito();
+    final carta = cartaMap(
+        id: 1,
+        nombre: 'bandeja paisa',
+        precio: 45.500,
+        imagen: 'Comida-típica-de-Colombia-Platos-Imprescindibles.jpg',
+        descripcion: 'preparado...');
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    //carrito.agregarItem(Item(carta), 1);
+    //carrito.agregarItem(producto_id, nombre, precio, unidad, imagen, cantidad)
+    carrito.items == carta;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    expect(carrito.incrementarCatidadItem.toString(), 1);
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Eliminar del carrito', () {
+    final carrito = Carrito();
+    final carta = cartaMap(
+        id: 1,
+        nombre: 'bandeja paisa',
+        precio: 45.500,
+        imagen: 'Comida-típica-de-Colombia-Platos-Imprescindibles.jpg',
+        descripcion: 'preparado...');
+
+    //carrito.agregarItem(producto_id, nombre, precio, unidad, imagen, cantidad);
+    //carrito.decrementarCantidadItem(carta);
+
+    expect(carrito.removerItem, 1);
+  });
+
+  test('Error carrito', () {
+    final carrito = Carrito();
+
+    carrito.items == null;
   });
 }
